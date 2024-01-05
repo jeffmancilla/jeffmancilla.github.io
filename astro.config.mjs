22e6471579d3b1f1803505f3c215c1fc6d7e0bc9
Jeff Mancilla <jeffmancilla@outlook.com>
2024-01-04 20:33:56 -0600

    fix: add gh-pages config in astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,7 @@ export default defineConfig({
   output: 'hybrid',
   integrations: [svelte(), tailwind({
     applyBaseStyles: false
-  }), react()]
+  }), react()],
+  site: 'https://jeffmancilla.github.io',
+
 });
