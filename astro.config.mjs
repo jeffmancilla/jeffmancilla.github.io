09aa94a0d4f9e2972c5fad0774d0ea5b813cfeda
Jeff Mancilla <jeffmancilla@outlook.com>
2024-01-25 17:05:07 -0600

    fix: add partytown, fix analytics build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,36 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import remarkGfm from 'remark-gfm';
-import remarkSmartypants from 'remark-smartypants';
-import rehypeExternalLinks from 'rehype-external-links';
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import remarkGfm from "remark-gfm"
+import remarkSmartypants from "remark-smartypants"
+import rehypeExternalLinks from "rehype-external-links"
+import tailwind from "@astrojs/tailwind"
 
-import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jeffmancilla.github.io',
-  integrations: [mdx(), tailwind()],
-  markdown: {
-    shikiConfig: {
-      theme: 'nord'
-    },
-    remarkPlugins: [remarkGfm, remarkSmartypants],
-    rehypePlugins: [[rehypeExternalLinks, {
-      target: '_blank'
-    }]]
-  }
-});
+	site: "https://jeffmancilla.github.io",
+	integrations: [
+		mdx(),
+		tailwind(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
+	markdown: {
+		shikiConfig: {
+			theme: "nord",
+		},
+		remarkPlugins: [remarkGfm, remarkSmartypants],
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					target: "_blank",
+				},
+			],
+		],
+	},
+})
